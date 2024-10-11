74adbe94c8af253d81b49d09aa13f19a1350adf8
Ashour Abdal <33664687+ash-js@users.noreply.github.com>
2024-10-12 01:29:50 +1100

    Del weather controller and adding 2 Villas

---
+++ Beautiful_Villa_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,18 @@
+﻿using Beautiful_Villa_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beautiful_Villa_API.Controllers
+{
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        public IEnumerable<Villa> GetVillas() 
+        {
+            return new List<Villa>()
+            {
+                new Villa() {Id=1, Name="Pool View" },
+                new Villa() {Id=2, Name="Beach View" }
+            };
+        }   
+    }
+}
